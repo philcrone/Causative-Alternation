68b220ccb48bc8b9d3f96f8d8ad959db247c972b
Phil Crone <philip.crone@gmail.com>
2013-12-15 21:08:30 -0500

    Major changes to template.js. This now provides the basic functionality of the experiment. Known issue with color change animation.

--- template.js
+++ template.js
@@ -1,4 +1,71 @@
-var numberOfQuestions = 10; // or if you have a fixed stim set, it may make sense to eliminate this variable and use its length as the counter
+var numberOfQuestions = 6; // or if you have a fixed stim set, it may make sense to eliminate this variable and use its length as the counter
+var exp_delay = 2500;
+var anim_time = 1000;
+
+var sentence_types = ["intras_pos","intrans_neg","trans_pos","trans_neg","peri_pos","peri_neg"];
+var exp_sentences = shuffle(sentence_types);
+
+var verbs = ["cheem","dax","zook","blonk","plaz","brilt"]
+var exp_verbs = shuffle(verbs);
+
+var animations = [fill, grow, shrink, glow, disappear, change_color];
+var exp_animations = shuffle(animations);
+
+function fill(element,delay_time){
+	var fill_anim = Raphael.animation({fill:element.attr("stroke")}, anim_time);
+	element.animate(fill_anim.delay(delay_time))
+}
+
+function grow(element, delay_time){
+	var grow_anim = Raphael.animation({transform:element.attr("transform")+"s2"}, anim_time);
+	element.animate(grow_anim.delay(delay_time));
+}
+
+function shrink(element, delay_time){
+	var grow_anim = Raphael.animation({transform:element.attr("transform")+"s0.25"}, anim_time);
+	element.animate(grow_anim.delay(delay_time));
+}
+
+function change_color(element, delay_time){
+	var element_color = Raphael.getRGB(element.attr("stroke")).toString();
+	element_color = element_color.replace("#","");
+	hexnum = element_color.toUpperCase();
+  	var splitnum = hexnum.split("");
+  	var resultnum = "";
+  	var simplenum = "FEDCBA9876".split("");
+  	var complexnum = new Array();
+  	complexnum.A = "5";
+  	complexnum.B = "4";
+  	complexnum.C = "3";
+  	complexnum.D = "2";
+  	complexnum.E = "1";
+  	complexnum.F = "0";
+	
+	for(i=0; i<6; i++){
+    if(!isNaN(splitnum[i])) {
+      resultnum += simplenum[splitnum[i]]; 
+    } else if(complexnum[splitnum[i]]){
+      resultnum += complexnum[splitnum[i]]; 
+    } else {
+      alert("Hex colors must only include hex numbers 0-9, and A-F");
+      return false;
+    }
+  	};
+  	
+  	var color_anim = Raphael.animation({stroke:"#"+resultnum}, anim_time);
+  	element.animate(color_anim.delay(delay_time));
+}
+
+function glow(element, delay_time){
+	var glow_stuff = element.glow({opacity:-0.05, width:50, color:element.attr("stroke")});
+	var glow_anim = Raphael.animation({opacity:0.05}, anim_time);
+	glow_stuff.animate(glow_anim.delay(delay_time))
+}	
+
+function disappear(element, delay_time){
+	var disappear_anim = Raphael.animation({"stroke-opacity":0}, anim_time);
+	element.animate(disappear_anim.delay(delay_time))
+}
 
 $(document).ready(function() {
     showSlide("instructions");
@@ -67,100 +134,94 @@ var experiment = {
 			qdata = {};
 			$("#error").hide();
 			var judgment = false;
-			var paper = Raphael("canvas", 300, 300);
-			var coordinates = shuffle([[60,60],[240,60],[150,240]])
-			var red_circle = paper.circle(coordinates[0][0], coordinates[0][1], 50);
-			var blue_circle = paper.circle(coordinates[1][0], coordinates[1][1], 50);
-			var green_circle = paper.circle(coordinates[2][0], coordinates[2][1], 50);
-			red_circle.attr("stroke", "red");
-			red_circle.attr("stroke-width", 5);
-			blue_circle.attr("stroke", "blue");
-			blue_circle.attr("stroke-width", 5);
-			green_circle.attr("stroke", "green");
-			green_circle.attr("stroke-width", 5);
-			var circles = [red_circle, green_circle, blue_circle];
+			var paper = Raphael("canvas", 450, 450);
 
-			function fill(element,delay_time){
-				var fill_anim = Raphael.animation({fill:element.attr("stroke")}, 1000);
-				element.animate(fill_anim.delay(delay_time))
+			var coordinates = [[150,150],[300,150],[225,300]];
+			var exp_coordinates = shuffle(coordinates);
+
+			var colors = ["red","blue","green"];
+			var exp_colors = shuffle(colors);
+
+			var shapes = ["circle", "square", "triangle"];
+			var exp_shapes = shuffle(shapes);
+
+			var obj_1;
+			var obj_2;
+			var obj_3;
+			var objs = [obj_1, obj_2, obj_3];
+
+			for (var i=0; i < objs.length;i++){
+				if (exp_shapes[i] == "circle"){
+					objs[i] = paper.circle(0,0,50);
+				}
+				else if (exp_shapes[i] == "square"){
+					objs[i] = paper.rect(-44, -44,88,88,0);
+				}
+				else if (exp_shapes[i] == "triangle"){
+					objs[i] = paper.path("M-50,-43.5L50,-43.5L0,43.5Z");
+				}
+				objs[i].data('shape',exp_shapes[i]);
+				objs[i].data('color',exp_colors[i]);
+				objs[i].transform("t" + exp_coordinates[i][0] + "," + exp_coordinates[i][1]);
+				objs[i].attr("stroke-width", 7);
+				objs[i].attr("stroke", exp_colors[i]);
+				}
+
+			var elements = shuffle(objs);
+
+			function move_to(){
+				var move = Raphael.animation({transform:elements[1].attr("transform")},anim_time, "bounce", move_back());
+				elements[0].animate(move.delay(exp_delay));
 			}
 
-			function intrans (animation, element){
-				animation(element,0);
+			function move_back(){
+				console.log(elements[0].attr("transform"));
+				console.log(elements[1].attr("transform"));
+				var move = Raphael.animation({transform:elements[0].attr("transform")},anim_time, "bounce");
+				elements[0].animate(move);
 			}
 
-			function trans (animation, elements){
+			function intrans (animation){
+				animation(elements[0],exp_delay);
+			}
+
+			function trans (animation){
 				var element1 = elements[0];
 				var element2 = elements[1];
-				animation(element1,0);
-				animation(element2,1000);
-				var point1;
-				var point2;
-				if (element1.attr("cy") == element2.attr("cy")) {
-					if (element1.attr("cx") < element2.attr("cx")) {
-						point1 = (element1.attr("cx") + element1.attr("r") + 10) + "," + element1.attr("cy");
-						point2 = (element2.attr("cx") - element2.attr("r") - 10) + "," + element2.attr("cy");
-					} else {
-						point1 = (element1.attr("cx") - element1.attr("r") - 10) + "," + element1.attr("cy");
-						point2 = (element2.attr("cx") + element2.attr("r") + 10) + "," + element2.attr("cy");
-					}
-				} else {
-					if (element1.attr("cy") < element2.attr("cy")) {
-						if (element1.attr("cx") < element2.attr("cx")) {
-						point1 = (element1.attr("cx") + element1.attr("r") - 10) + "," + (element1.attr("cy") + element1.attr("r") - 10);
-						point2 = element2.attr("cx") + "," + (element2.attr("cy") - element2.attr("r") - 10);
-						} else {
-						point1 = (element1.attr("cx") - element1.attr("r") + 10) + "," + (element1.attr("cy") + element1.attr("r") - 10);
-						point2 = element2.attr("cx") + "," + (element2.attr("cy") - element2.attr("r") - 10);
-						}
-					} else {
-						if (element2.attr("cx") < element1.attr("cx")) {
-						point1 = element1.attr("cx") + "," + (element1.attr("cy") - element1.attr("r") - 10);
-						point2 = (element2.attr("cx") + element2.attr("r") - 10) + "," + (element2.attr("cy") + element2.attr("r") - 10);
-						} else {
-						point1 = element1.attr("cx") + "," + (element1.attr("cy") - element1.attr("r") - 10);
-						point2 = (element2.attr("cx") - element2.attr("r") + 10) + "," + (element2.attr("cy") + element2.attr("r") - 10);
-						}
-					}
-				}
-				var path = paper.path("M"+point1+"L"+point2);
-				path.attr("stroke-width", 3);
-				path.attr("arrow-end","classic");
+				element1.toFront();
+				move_to();
+				animation(element2,anim_time+exp_delay);
 			}
 
-			function simultaneous (animation, elements){
-				element1 = elements[0];
-				element2 = elements[1];
-				animation(element1,0);
-				animation(element2,0);
+			if (exp_sentences[num] == "intras_pos"){
+				intrans(exp_animations[num]);
+				$("#questiontxt").html('The '+ elements[0].data("color") + " " + elements[0].data("shape") + " " + exp_verbs[num]+"ed."); 
 			}
-
-			function get_elements () {
-				var causer = circles[Math.floor(Math.random() * circles.length)];
-				var new_circles  = []
-				for (var i = 0; i < circles.length; i++){
-					if (circles[i] != causer){
-						new_circles.push(circles[i]);
-						}
-					}
-				var causee = new_circles[Math.floor(Math.random() * new_circles.length)];
-				return [causer, causee];
+			else if (exp_sentences[num] == "intras_neg"){
+				intrans(exp_animations[num]);
+				$("#questiontxt").html('The '+ elements[1].data("color") + " " + elements[1].data("shape") + " didn't " + exp_verbs[num] +"."); 
 			}
-			var causation_types = [intrans, trans, simultaneous];
-			var causation_type = causation_types[Math.floor(Math.random() * causation_types.length)];
-			var elements = get_elements();
-			if (causation_type == intrans){
-				causation_type(fill, elements[0]);
+			else if (exp_sentences[num] == "trans_pos"){
+				trans(exp_animations[num]);
+				$("#questiontxt").html('The '+ elements[0].data("color") + " " + elements[0].data("shape") + " " + exp_verbs[num] + "ed the "+ elements[1].data("color") + " " + elements[1].data("shape") + "."); 
 			}
-			else {
-				causation_type(fill, elements);
+			else if (exp_sentences[num] == "trans_neg"){
+				trans(exp_animations[num]);
+				$("#questiontxt").html('The '+ elements[2].data("color") + " " + elements[2].data("shape") + " didn't " + exp_verbs[num] + " the "+ elements[1].data("color") + " " + elements[1].data("shape") + "."); 
+			}
+			else if (exp_sentences[num] == "peri_pos"){
+				trans(exp_animations[num]);
+				$("#questiontxt").html('The '+ elements[0].data("color") + " " + elements[0].data("shape") + " caused the "+ elements[1].data("color") + " " + elements[1].data("shape") + " to " + exp_verbs[num]+"."); 
+			}
+			else if (exp_sentences[num] == "peri_neg"){
+				trans(exp_animations[num]);
+				$("#questiontxt").html('The '+ elements[2].data("color") + " " + elements[2].data("shape") + " didn't cause the "+ elements[1].data("color") + " " + elements[1].data("shape") + " to " + exp_verbs[num]+".");
 			}
 			var response;
 			$(".rating").change(function() {
 				response = $(this).attr("value");
 				judgment = true;
         	});
-			$("#questiontxt").html('This is another test.'); 
 	    	$("#continue").click(function() {
 	    		if (!judgment) { // test for answer meeting relevant parameters -- e.g., all questions answered
 	    			// if no, show some text saying so and ask them to answer appropriately
